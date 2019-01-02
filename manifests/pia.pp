eb1a6f6c91fd1ef1b9860ec845f8b2bc9c6c9c01
Dan Iverson <dan.iverson@minneapolismn.gov>
2019-01-02 13:44:13 -0600

    Using file_line to add weblogic.xml filter

--- manifests/pia.pp
+++ manifests/pia.pp
@@ -9,14 +9,103 @@ class io_erpfirewall::pia () inherits io_erpfirewall {
   $pia_domain_list.each |$domain_name, $pia_domain_info| {
     $deploy_location = "${ps_config_home}/webserv/${domain_name}/applications/peoplesoft"
 
-    file {"ERPFirewall-PIA-${domain_name}":
+    file {"ERPFirewall-PIA-lib-${domain_name}":
       ensure  => file,
-      source  => $deploy_source,
+      source  => "${deploy_source}/lib",
       recurse => true,
-      path    => $deploy_location,
+      path    => "${deploy_location}/lib",
       owner   => $psft_runtime_user_name,
       group   => $psft_runtime_group_name,
       mode    => '0644',
     }
+
+    file {"ERPFirewall-PIA-WEB-INF-gsdocs-${domain_name}":
+      ensure  => file,
+      source  => "${deploy_source}/PORTAL.war/WEB-INF/gsdocs",
+      recurse => true,
+      path    => "${deploy_location}/PORTAL.war/WEB-INF/gsdocs",
+      owner   => $psft_runtime_user_name,
+      group   => $psft_runtime_group_name,
+      mode    => '0644',
+    }
+
+    file {"ERPFirewall-PIA-WEB-INF-lib-${domain_name}":
+      ensure  => file,
+      source  => "${deploy_source}/PORTAL.war/WEB-INF/lib",
+      recurse => true,
+      path    => "${deploy_location}/PORTAL.war/WEB-INF/lib",
+      owner   => $psft_runtime_user_name,
+      group   => $psft_runtime_group_name,
+      mode    => '0644',
+    }
+
+    # $filter_content = {
+    #   'filter-name' => 'gs_erp_firewall',
+    #   'filter-class'=>'com.greyheller.firewall.Psfw',
+    #   'async-supported' => true,
+    #   'init-param' => {
+    #     'param-name' => 'failopen',
+    #     'param-value' => false
+    #     }
+    #   }
+
+    # notify { "Filter Content Hash: ${filter_content}":}
+
+    # xml_fragment {"ERPFirewall-web-xml-${domain_name}":
+    #   ensure  => $ensure,
+    #   xpath   => "/web-app/filter/filter-name[text()='gs_erp_firewall']",
+    #   path    => "${deploy_location}/PORTAL.war/WEB-INF/web.xml",
+    #   content => {
+    #     'filter-name'     => 'gs_erp_firewall',
+    #     'filter-class'    =>'com.greyheller.firewall.Psfw',
+    #     'async-supported' => true,
+    #     'init-param'      => {
+    #       'param-name'  => 'failopen',
+    #       'param-value' => false
+    #       }
+    #     },
+    # }
+
+    $filter_match = '    <filter-name>gs_erp_firewall</filter-name>'
+    $filter_line = "
+  <filter>
+    <filter-name>gs_erp_firewall</filter-name>
+    <filter-class>com.greyheller.firewall.Psfw</filter-class>
+    <async-supported>true</async-supported>
+    <init-param>
+      <param-name>failopen</param-name>
+      <param-value>false</param-value>
+    </init-param>
+  </filter>
+  <filter-mapping>
+    <filter-name>gs_erp_firewall</filter-name>
+    <url-pattern>/*</url-pattern>
+  </filter-mapping>
+"
+
+    file_line { "ERPFirewall-web-xml-filter-${domain}":
+      path    => "${deploy_location}/PORTAL.war/WEB-INF/web.xml",
+      line    => $filter_line,
+      match   => $filter_match,
+      after   => '  <!-- <distributable/> -->',
+      replace => false,
+    }
+
+#     $filter_mapping_match = "    <filter-name>gs_erp_firewall</filter-name>"
+#     $filter_mapping_line = "
+#   <filter-mapping>
+#     <filter-name>gs_erp_firewall</filter-name>
+#     <url-pattern>/*</url-pattern>
+#   </filter-mapping>
+# "
+
+#     file_line { "ERPFirewall-web-xml-filter-mapping-${domain}":
+#       path    => "${deploy_location}/PORTAL.war/WEB-INF/web.xml",
+#       line    => $filter_mapping_line,
+#       match   => $filter_mapping_match,
+#       after   => '  </filter-mapping>',
+#       replace => false,
+#     }
+
   }
 }
