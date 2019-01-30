a7c0bd07fe0085237b66724d640d62f475f0075e
Dan Iverson <dan.iverson@minneapolismn.gov>
2019-01-30 09:58:02 -0600

    Add failopen=true

--- manifests/pia.pp
+++ manifests/pia.pp
@@ -11,35 +11,42 @@ class io_erpfirewall::pia () inherits io_erpfirewall {
     $udomain_name = upcase($domain_name)
 
     case $library_platform {
-      default: {
-        exec { 'install_erpfirewall':
-          command => "/usr/bin/su -m -s /bin/bash - ${psft_runtime_user_name} -c \"${archive_location}/ERP_Firewall/WebServer/Unix/gh_firewall_web.bin ${ps_config_home} ${domain_name}\"",
-          creates => "${ps_config_home}/webserv/${domain_name}/applications/peoplesoft/PORTAL.war/WEB-INF/gsdocs",
-        }
-
-        file { "${ps_config_home}/webserv/${domain_name}/applications/peoplesoft/PORTAL.war/WEB-INF/lib/psjoa.jar" :
-          source  => "${ps_home_location}/appserv/classes/psjoa.jar",
-          owner   => $psft_runtime_user_name,
-          mode    => '0755',
-          require => Exec['install_erpfirewall'],
-        }
+    default: {
+      exec { 'install_erpfirewall':
+        command => "/usr/bin/su -m -s /bin/bash - ${psft_runtime_user_name} -c \"${archive_location}/ERP_Firewall/WebServer/Unix/gh_firewall_web.bin ${ps_config_home} ${domain_name}\"",
+        creates => "${ps_config_home}/webserv/${domain_name}/applications/peoplesoft/PORTAL.war/WEB-INF/gsdocs",
       }
 
-      'Windows': {
-        file { "${ps_config_home}/webserv/${domain_name}/applications/peoplesoft/PORTAL.war/WEB-INF/lib/psjoa.jar" :
-          source  => "${ps_home_location}/class/psjoa.jar",
-          owner   => $psft_runtime_user_name,
-          mode    => '0755',
-        } ->
-        exec { 'copy_erpfirewall_installer':
-          command  => "copy-item ${archive_location}/ERP_Firewall/WebServer/Windows/gh_firewall_web.exe c:/temp",
-          creates  => "c:/temp/gh_firewall_web.exe",
-          provider => powershell,
-        } -> 
-        exec { 'install_erpfirwall':
-          command  => "& \"c:/temp/gh_firewall_web.exe\" /log=\"c:/temp/erpfirewall-webserver-installation.log\" /verysilent /suppressmsgboxes /pshome=\"${ps_config_home}\" /piadomain=\"${udomain_name}\"",
-          creates  => "${ps_config_home}/webserv/${domain_name}/applications/peoplesoft/PORTAL.war/WEB-INF/gsdocs",
-          provider => powershell,
+      file { "${ps_config_home}/webserv/${domain_name}/applications/peoplesoft/PORTAL.war/WEB-INF/lib/psjoa.jar" :
+        source  => "${ps_home_location}/appserv/classes/psjoa.jar",
+        owner   => $psft_runtime_user_name,
+        mode    => '0755',
+        require => Exec['install_erpfirewall'],
+      }
+    }
+
+    'Windows': {
+      file { "${ps_config_home}/webserv/${domain_name}/applications/peoplesoft/PORTAL.war/WEB-INF/lib/psjoa.jar" :
+        source  => "${ps_home_location}/class/psjoa.jar",
+        owner   => $psft_runtime_user_name,
+        mode    => '0755',
+      } ->
+      exec { 'copy_erpfirewall_installer':
+        command  => "copy-item ${archive_location}/ERP_Firewall/WebServer/Windows/gh_firewall_web.exe c:/temp",
+        creates  => "c:/temp/gh_firewall_web.exe",
+        provider => powershell,
+      } -> 
+      exec { 'install_erpfirwall':
+        command  => "& \"c:/temp/gh_firewall_web.exe\" /log=\"c:/temp/erpfirewall-webserver-installation.log\" /verysilent /suppressmsgboxes /pshome=\"${ps_config_home}\" /piadomain=\"${udomain_name}\"",
+        creates  => "${ps_config_home}/webserv/${domain_name}/applications/peoplesoft/PORTAL.war/WEB-INF/gsdocs",
+        provider => powershell,
+      } ->
+      xml_fragment { "${domain_name}_fail_open":
+        path        => "C:/hosts.xml",
+        ensure      => 'present',
+        xpath       => "/web-app/filter[filter-name='gs_erp_firewall']/init-param/param-value",
+        content     => {
+            value   => "true"
         }
       }
     } # case platform
