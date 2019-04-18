10874e2d961cbca434f67827fb27bb6b42aee289
Dan Iverson <dan@daniverson.com>
2019-04-18 10:22:00 -0500

    Use installer programs to deploy (#1)
    * Switching to use Installer executables
    
    * Switched to case statements
    
    * Added some debugging messages to appserver.pp
    
    * Add missing $ in pia.pp
    
    * Can't execute setup from UNC, fix psjoa path on Windows
    
    * Change  to
    
    * Fix command line for installing PIA filter
    
    * Separate file copy and installation setp
    
    * Add failopen=true
    
    * Add missing curly bracket
    
    * Make failopen a string
    
    * Fix path for failopen update
    
    * Ensure c:/temp is created
    
    * Add redeploy_firewall fact
    
    * Fix redeploy logic
    
    * Adding proper variable passing
    
    * Use /bin/su instead of /usr/bin/su

--- manifests/appserver.pp
+++ manifests/appserver.pp
@@ -4,23 +4,31 @@
 class io_erpfirewall::appserver () inherits io_erpfirewall {
   notify { 'Deploying appserver files for ERP Firewall': }
 
-  if $use_ps_cust_home == true {
-    $deploy_location = "${ps_cust_home_location}/class"
-  } else {
-    $deploy_location = "${ps_home_location}/class"
+  $appserver_domain_list.each |$domain_name, $appserver_domain_info| {
+    notify {"${domain_name} App Deploy Location: ${app_deploy_location}": }
+
+    case $library_platform {
+      default: {
+        exec { "Unix ERP Firewall Application Server Install: ${domain_name} ${app_deploy_location}":
+          command => "${archive_location}/ERP_Firewall/AppServer/Unix/gh_fwappserv.bin ${app_deploy_location}",
+          creates => "${app_deploy_location}/appserv/classes/gs-util.jar",
+          user    => $psft_install_user_name,
+        }
+      }
+      'Windows': {
+        exec { "Windows ERP Firewall Application Server Install: ${domain_name} ${app_deploy_location}":
+          command => "copy-item ${archive_location}/ERP_Firewall/AppServer/Windows/setup.exe \${Env:temp};
+                \${Env:temp}/setup.exe `
+                /log=\"\${Env:TEMP}/appserver-installation.log\" `
+                /verysilent `
+                /suppressmsgboxes `
+                /pshome=\"${app_deploy_location}\"",
+          creates => "${app_deploy_location}/classes/gs-util.jar",
+          provider => powershell,
+        }
+      }
+    } # end case ${library_platform}
+
   }
 
-  $deploy_source = "${library_base}/appserver/${library_platform}"
-
-  $appserver_domain_list.each |$domain_name, $appserv_domain_info| {
-    file {"ERPFirewall-AppServer-${domain_name}":
-      ensure  => file,
-      source  => $deploy_source,
-      recurse => true,
-      path    => $deploy_location,
-      owner   => $psft_runtime_user_name,
-      group   => $psft_runtime_group_name,
-      mode    => '0644',
-    }
-  }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,18 @@
 class io_erpfirewall (
   $ensure                    = hiera('ensure', 'present'),
-  $psft_install_user_name    = hiera('psft_install_user_name', undef),
-  $oracle_install_group_name = hiera('oracle_install_group_name', undef),
-  $pia_domain_list           = hiera_hash('pia_domain_list', undef),
-  $appserver_domain_list     = hiera_hash('appserver_domain_list', undef),
+  $psft_runtime_user_name    = hiera('psft_runtime_user_name', 'psadm2'),
+  $oracle_install_group_name = hiera('oracle_install_group_name', 'oinstall'),
+  $pia_domain_list           = hiera('pia_domain_list', undef),
+  $appserver_domain_list     = hiera('appserver_domain_list', undef),
   $library_base              = undef,
   $pia                       = undef,
   $appserver                 = undef,
-  $use_ps_cust_home          = false,
+  $app_deploy_location       = undef,
   $ps_home_location          = hiera('ps_home_location', undef),
   $ps_cust_home_location     = hiera('ps_cust_home_location', undef),
   $ps_config_home            = hiera('ps_config_home', undef),
+  $archive_location          = hiera('archive_location', undef),
+  $redeploy_firewall         = hiera('redeploy_firewall', undef)
 ) {
 
   case $::osfamily {
