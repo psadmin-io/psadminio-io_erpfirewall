cf85c89bcf166c595ad432d5948b189298631e99
Dan Iverson <dan@daniverson.com>
2021-12-07 08:01:53 -0600

    Update app server install to use PS_CFG_HOME

--- manifests/appserver.pp
+++ manifests/appserver.pp
@@ -1,29 +1,35 @@
 # Class: io_erpfirewall::appserver
 #
 #
-class io_erpfirewall::appserver () inherits io_erpfirewall {
+class io_erpfirewall::appserver (
+  $psft_runtime_user_name       = $io_erpfirewall::psft_runtime_user_name,
+  $appserver_domain_list        = $io_erpfirewall::appserver_domain_list,
+  $library_platform             = $io_erpfirewall::library_platform,
+  $archive_location             = $io_erpfirewall::archive_location,
+  $ps_config_home               = $io_erpfirewall::ps_config_home,
+) inherits io_erpfirewall {
   notify { 'Deploying appserver files for ERP Firewall': }
 
   $appserver_domain_list.each |$domain_name, $appserver_domain_info| {
-    notify {"${domain_name} App Deploy Location: ${app_deploy_location}": }
+    notify {"${domain_name} App Deploy Location: ${ps_config_home}": }
 
     case $library_platform {
       default: {
-        exec { "Unix ERP Firewall Application Server Install: ${domain_name} ${app_deploy_location}":
-          command => "${archive_location}/AppServer/Unix/asp_app.bin ${app_deploy_location}",
-          creates => "${app_deploy_location}/appserv/classes/gs-util.jar",
-          user    => $psft_install_user_name,
+        exec { "Unix ERP Firewall Application Server Install: ${domain_name} ${ps_config_home}":
+          command => "${archive_location}/AppServer/Unix/asp_app.bin ${ps_config_home}",
+          creates => "${ps_config_home}/appserv/classes/gs-util.jar",
+          user    => $psft_runtime_user_name,
         }
       }
       'Windows': {
-        exec { "Windows ERP Firewall Application Server Install: ${domain_name} ${app_deploy_location}":
-          command => "copy-item ${archive_location}/AppServer/Windows/asp_app.exe \${Env:temp};
+        exec { "Windows ERP Firewall Application Server Install: ${domain_name} ${ps_config_home}":
+          command  => "copy-item ${archive_location}/AppServer/Windows/asp_app.exe \${Env:temp};
                 \${Env:temp}/asp_app.exe `
                 /log=\"\${Env:TEMP}/appserver-installation.log\" `
                 /verysilent `
                 /suppressmsgboxes `
-                /pshome=\"${app_deploy_location}\"",
-          creates => "${app_deploy_location}/classes/gs-util.jar",
+                /pshome=\"${ps_config_home}\"",
+          creates  => "${ps_config_home}/classes/gs-util.jar",
           provider => powershell,
         }
       }
