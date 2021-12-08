a6e9f7e452d644616489b88def3ca3e8cf19f5cf
Dan Iverson <dan@daniverson.com>
2021-12-08 10:20:33 -0600

    Switching app install back to PS_HOME

--- manifests/appserver.pp
+++ manifests/appserver.pp
@@ -6,30 +6,30 @@ class io_erpfirewall::appserver (
   $appserver_domain_list        = $io_erpfirewall::appserver_domain_list,
   $library_platform             = $io_erpfirewall::library_platform,
   $archive_location             = $io_erpfirewall::archive_location,
-  $ps_config_home               = $io_erpfirewall::ps_config_home,
+  $ps_home_location             = $io_erpfirewall::ps_home_location,
 ) inherits io_erpfirewall {
   notify { 'Deploying appserver files for ERP Firewall': }
 
   $appserver_domain_list.each |$domain_name, $appserver_domain_info| {
-    notify {"${domain_name} App Deploy Location: ${ps_config_home}": }
+    notify {"${domain_name} App Deploy Location: ${ps_home_location}": }
 
     case $library_platform {
       default: {
-        exec { "Unix ERP Firewall Application Server Install: ${domain_name} ${ps_config_home}":
-          command => "${archive_location}/AppServer/Unix/asp_app.bin ${ps_config_home}",
-          creates => "${ps_config_home}/appserv/classes/gs-util.jar",
+        exec { "Unix ERP Firewall Application Server Install: ${domain_name} ${ps_home_location}":
+          command => "${archive_location}/AppServer/Unix/asp_app.bin ${ps_home_location}",
+          creates => "${ps_home_location}/appserv/classes/gs-util.jar",
           user    => $psft_runtime_user_name,
         }
       }
       'Windows': {
-        exec { "Windows ERP Firewall Application Server Install: ${domain_name} ${ps_config_home}":
+        exec { "Windows ERP Firewall Application Server Install: ${domain_name} ${ps_home_location}":
           command  => "copy-item ${archive_location}/AppServer/Windows/asp_app.exe \${Env:temp};
                 \${Env:temp}/asp_app.exe `
                 /log=\"\${Env:TEMP}/appserver-installation.log\" `
                 /verysilent `
                 /suppressmsgboxes `
-                /pshome=\"${ps_config_home}\"",
-          creates  => "${ps_config_home}/classes/gs-util.jar",
+                /pshome=\"${ps_home_location}\"",
+          creates  => "${ps_home_location}/classes/gs-util.jar",
           provider => powershell,
         }
       }
